e44684635c184369cad7f3e3eb5a960e07fcfa7b
Ian Altemus <ianaltemus@Ians-Air.lan>
2026-03-16 17:34:34 -0700

    feat: add VPC module with public/private subnets, igw, nat gw, and route tables

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "main" {
-  cidr_block = "var.vpc_cidr"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    Name = "$var.project_name-${var.environment}-vpc"
+    Name = "${var.project_name}-${var.environment}-vpc"
     Environment = var.environment
     Project = var.project_name
   }
@@ -60,7 +60,7 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "main" {
-  allocation_id = aws.eip.nat.id
+  allocation_id = aws_eip.nat.id
   subnet_id = aws_subnet.public[0].id
 
   tags = {
@@ -70,4 +70,44 @@ resource "aws_nat_gateway" "main" {
   }
 
   depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "${var.project_name}-${var.environment}-public-rt"
+        Environment = var.environment
+        Project = var.project_name
+    }
+}
+
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.main.id
+
+        }
+    tags = {
+        Name = "${var.project_name}-${var.environment}-private-rt"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    count = length(var.public_subnet_cidrs)
+    subnet_id = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+    count = length(var.private_subnet_cidrs)
+    subnet_id = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private.id
 }
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,7 +1,7 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type = string
-  default = 10.0.0.0/16
+  default = "10.0.0.0/16"
 }
 
 variable "environment" {
