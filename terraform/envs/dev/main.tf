d729ca0f5b8f893f87c9290325657a7aba833a89
Ian Altemus <YOUR_EMAIL_HERE>
2026-04-08 12:43:08 -0700

    added the nlb tgt group to the fargate task 2

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -76,6 +76,7 @@ module "fargate" {
   followup_lambda_arn      = module.followup_lambda.lambda_arn
   scheduler_role_arn       = module.followup_lambda.scheduler_role_arn
   target_group_arn         = module.alb.target_group_arn
+  nlb_target_group_arn     = module.nlb.nlb_target_group_arn
   bedrock_model_arn        = "arn:aws:bedrock:us-east-1:894943009636:inference-profile/us.anthropic.claude-haiku-4-5-20251001-v1:0"
   google_sheets_secret_arn = "arn:aws:secretsmanager:us-east-1:894943009636:secret:secretive-nail-bar/dev/google-sheets-credentials-IM5gxg"
 
